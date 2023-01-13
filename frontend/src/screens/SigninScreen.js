3f2e76d16cc7132600a174cc3f5226f9ff108a5a
Yosi7708 <yosi7708@gmail.com>
2023-01-13 04:01:10 +0200

    mongoDB added

--- frontend/src/screens/SigninScreen.js
+++ frontend/src/screens/SigninScreen.js
@@ -11,17 +11,17 @@ export default function SigninScreen() {
   return (
     <Container className="small-container">
       <Helmet>
-        <title>Sign in</title>
+        <title>Sign In</title>
       </Helmet>
       <h1 className="my-3">Sign In</h1>
       <Form>
         <Form.Group className="mb-3" controlId="email">
           <Form.Label>Email</Form.Label>
-          <Form.Control type="email" required></Form.Control>
+          <Form.Control type="email" required />
         </Form.Group>
         <Form.Group className="mb-3" controlId="password">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" required></Form.Control>
+          <Form.Control type="password" required />
         </Form.Group>
         <div className="mb-3">
           <Button type="submit">Sign In</Button>
